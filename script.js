7ce696d0df9471a192fe43b61847593d4c027ecd
Giri prasath V <giriprasath2525@gmail.com>
2024-01-29 10:59:51 +0530

    changes in js

--- script.js
+++ script.js
@@ -10,15 +10,15 @@ let page = 1;
 
 async function searchImages(){
     inputData = inputElement.value;
-    const url  = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`
+    const url  = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
 
-    const response = await fetch(url)
-    const data = await response.json()
+    const response = await fetch(url);
+    const data = await response.json();
 
-    const results = data.results
+    const results = data.results;
 
     if (page===1){
-        searchResults.innerHTML=""
+        searchResults.innerHTML="";
     }
 
     results.map((result)=>{
@@ -55,8 +55,8 @@ formElement.addEventListener("submit", (event)=>{
     event.preventDefault;
     page = 1;
     searchImages();
-})
+});
 
 showMore.addEventListener("click", ()=>{
     searchImages();
-})
+});
