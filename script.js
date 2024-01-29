76e3177492ab601d641598082386c38bac6d3070
Giri prasath V <giriprasath2525@gmail.com>
2024-01-29 10:40:53 +0530

    javascript completed

---
+++ script.js
@@ -0,0 +1,62 @@
+const accessKey = "E0kYTWamVhaBvNm0omnRsw6b84yG9cbDlvtgfp_150s";
+
+const formElement = document.querySelector("form");
+const inputElement = document.getElementById("search-input");
+const searchResults = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more");
+
+let inputData = "";
+let page = 1;
+
+async function searchImages(){
+    inputData = inputElement.value;
+    const url  = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`
+
+    const response = await fetch(url)
+    const data = await response.json()
+
+    const results = data.results
+
+    if (page===1){
+        searchResults.innerHTML=""
+    }
+
+    results.map((result)=>{
+        const imageWrapper = document.createElement("div");
+        imageWrapper.classList.add("search-result");
+
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        imageLink.textContent = result.alt_description;
+
+
+        // append child
+
+        searchResults.appendChild(imageWrapper);
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+
+    });
+
+    page++;
+
+    if (page>1){
+        showMore.style.display = "block";
+    }
+}
+
+//event listener
+
+formElement.addEventListener("submit", (event)=>{
+    event.preventDefault;
+    page = 1;
+    searchImages();
+})
+
+showMore.addEventListener("click", ()=>{
+    searchImages();
+})
